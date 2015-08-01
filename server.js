3d9e7bb62f6d6ba640623442742c2f71121d3151
Eduardo <eduardopaucar1@gmail.com>
2015-08-01 14:18:56 +0900

    Database - port
    cambio de puerto por defecto

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ connect().use(serveStatic(__dirname)).listen(8070);
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
+  port     : 3306,
   user     : 'root',
   password : 'root',
   database : 'test'
